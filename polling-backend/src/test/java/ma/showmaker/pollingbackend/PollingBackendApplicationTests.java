e23bae061f20b05319cda90126ab2c08b457208e
healingfields <dihratene@gmail.com>
2022-10-12 08:53:36 +0100

    created the backend project

---
+++ polling-backend/src/test/java/ma/showmaker/pollingbackend/PollingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.showmaker.pollingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
