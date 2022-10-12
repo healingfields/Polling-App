f346363f35b1e91a0c1780744c49541b0c3ea051
healingfields <dihratene@gmail.com>
2022-10-12 09:14:47 +0100

    created the Vote model

---
+++ polling-backend/src/main/java/ma/showmaker/pollingbackend/model/Vote.java
@@ -0,0 +1,44 @@
+package ma.showmaker.pollingbackend.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "votes")
+public class Vote {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "poll_id", nullable = false)
+    private Poll poll;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "choice_id", nullable = false)
+    private Choice choice;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Poll getPoll() {
+        return poll;
+    }
+
+    public void setPoll(Poll poll) {
+        this.poll = poll;
+    }
+
+    public Choice getChoice() {
+        return choice;
+    }
+
+    public void setChoice(Choice choice) {
+        this.choice = choice;
+    }
+}
