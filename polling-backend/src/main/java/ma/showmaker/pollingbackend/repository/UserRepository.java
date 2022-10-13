2be91eb8ed009f50a537fc8b71de10922ab036b6
healingfields <dihratene@gmail.com>
2022-10-13 13:58:50 +0100

    created the user repository

---
+++ polling-backend/src/main/java/ma/showmaker/pollingbackend/repository/UserRepository.java
@@ -0,0 +1,22 @@
+package ma.showmaker.pollingbackend.repository;
+
+import ma.showmaker.pollingbackend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByUsernameOrEmail(String username, String email);
+
+    List<User> findByIdIn(Long userIds);
+
+    Optional<User> findByUsername(String username);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
+}
