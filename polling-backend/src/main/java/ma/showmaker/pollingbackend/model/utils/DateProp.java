64e2cc87239556a3e814075629f5a7cb85ca0dc9
healingfields <dihratene@gmail.com>
2022-10-13 13:58:25 +0100

    used the auditing annotation for the createdAt and updatedAt attributes

--- polling-backend/src/main/java/ma/showmaker/pollingbackend/model/utils/DateProp.java
+++ polling-backend/src/main/java/ma/showmaker/pollingbackend/model/utils/DateProp.java
@@ -1,14 +1,20 @@
 package ma.showmaker.pollingbackend.model.utils;
 
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 import java.time.Instant;
 
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class DateProp implements Serializable {
-
+    @CreatedDate
     private Instant createdAt;
-
+    @LastModifiedDate
     private Instant updatedAt;
 
     public Instant getCreatedAt() {
