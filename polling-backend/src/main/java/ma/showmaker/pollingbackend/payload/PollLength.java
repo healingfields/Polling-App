1b4e41a9f3d76812ca1735fa35fe5f77b35772a9
healingfields <dihratene@gmail.com>
2022-10-13 18:51:19 +0100

    created Poll Length and request

---
+++ polling-backend/src/main/java/ma/showmaker/pollingbackend/payload/PollLength.java
@@ -0,0 +1,26 @@
+package ma.showmaker.pollingbackend.payload;
+
+import java.util.Date;
+
+public class PollLength {
+
+    private Integer days;
+
+    private Integer hours;
+
+    public Integer getDays() {
+        return days;
+    }
+
+    public void setDays(Integer days) {
+        this.days = days;
+    }
+
+    public Integer getHours() {
+        return hours;
+    }
+
+    public void setHours(Integer hours) {
+        this.hours = hours;
+    }
+}
---
+++ polling-backend/src/main/java/ma/showmaker/pollingbackend/payload/PollRequest.java
@@ -0,0 +1,38 @@
+package ma.showmaker.pollingbackend.payload;
+
+import org.apache.tomcat.jni.Poll;
+
+import java.util.List;
+
+public class PollRequest {
+
+    private String question;
+
+    private List<ChoiceRequest> choices;
+
+    private PollLength pollLength;
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public List<ChoiceRequest> getChoices() {
+        return choices;
+    }
+
+    public void setChoices(List<ChoiceRequest> choices) {
+        this.choices = choices;
+    }
+
+    public PollLength getPollLength() {
+        return pollLength;
+    }
+
+    public void setPollLength(PollLength pollLength) {
+        this.pollLength = pollLength;
+    }
+}
