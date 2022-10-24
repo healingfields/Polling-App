52b4467f81b5345606883c4a18bfd6b6cc520d31
healingfields <dihratene@gmail.com>
2022-10-24 21:10:13 +0100

    created the role repo

---
+++ polling-backend/src/main/java/ma/showmaker/pollingbackend/repository/RoleRepository.java
@@ -0,0 +1,12 @@
+package ma.showmaker.pollingbackend.repository;
+
+import ma.showmaker.pollingbackend.model.Role;
+import ma.showmaker.pollingbackend.model.RoleName;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(RoleName rolename);
+}
