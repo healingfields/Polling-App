298ad80ff8f06dba8eceecdbf6adc7a9ec966d82
healingfields <dihratene@gmail.com>
2022-10-30 21:56:26 +0100

    created an interface for stocking constants values

---
+++ polling-backend/src/main/java/ma/showmaker/pollingbackend/util/AppConstants.java
@@ -0,0 +1,8 @@
+package ma.showmaker.pollingbackend.util;
+
+public interface AppConstants {
+    String DEFAULT_PAGE_NUMBER = "0";
+    String DEFAULT_PAGE_SIZE = "10";
+
+    int MAX_PAGE_SIZE = 25;
+}
