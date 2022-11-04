1d3b45bd1500044be3461867e4d3e738b8b491a0
healingfields <dihratene@gmail.com>
2022-11-04 18:43:16 +0100

    created routing

--- polling-frontend/src/components/container/App.js
+++ polling-frontend/src/components/container/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import PollList from '../presentational/poll/pollList/PollList';
+import NewPoll from '../presentational/poll/newPoll/NewPoll';
+import Login from '../presentational/user/login/Login';
+import SignUp from '../presentational/user/signUp/SignUp';
+import Profile from '../presentational/user/profile/Profile';
+import PrivateRoute from '../common/privateRoute/PrivateRoute'
+import {createBrowserRouter, Route, RouterProvider} from 'react-router-dom'
+import { Layout} from 'antd';
+import { useState } from 'react';
 
+const {Header, Footer, Content} = Layout;
+
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <PollList/>,
+    index:true
+
+  },
+  {
+    path:"/login",
+    element:<Login/>
+  },
+  {
+    path:"/singup",
+    element:<SignUp/>
+  },
+  {
+    path:"users/:username",
+    element:<Profile/>
+  },
+  {
+    path:"/poll/new",
+    element:
+      <PrivateRoute>
+        <NewPoll/>
+      </PrivateRoute>
+  }
+])
 function App() {
+
+  const [currentUser, setCurrentUser] = useState();
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout className="App-container">
+      <Header>Header</Header>
+     
+
+     <Content>
+     <RouterProvider router={router}/>
+     </Content>
+
+     <Footer>footer</Footer>
+      
+    </Layout>
   );
 }
 
