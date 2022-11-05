699fdbe882c8b6386ba0b0b744ba899ad92bb48b
healingfields <dihratene@gmail.com>
2022-11-05 18:21:45 +0100

    created the privateRoute

--- polling-frontend/src/components/common/privateRoute/PrivateRoute.js
+++ polling-frontend/src/components/common/privateRoute/PrivateRoute.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import { redirect } from 'react-router-dom';
 
-export const PrivateRoute = () => {
-  return (
-    <div>PrivateRoute</div>
-  )
+export const PrivateRoute = ({isAuthenticated, children}) => {
+  if(!isAuthenticated){
+    redirect("/login");
+  }
+  return children;
 }
 export default PrivateRoute;
