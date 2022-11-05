7b8b9af996d9a481a04b12a78eb62493980f2e69
healingfields <dihratene@gmail.com>
2022-11-05 18:22:17 +0100

    created the login, logout functionnality

--- polling-frontend/src/components/container/App.js
+++ polling-frontend/src/components/container/App.js
@@ -5,9 +5,12 @@ import Login from '../presentational/user/login/Login';
 import SignUp from '../presentational/user/signUp/SignUp';
 import Profile from '../presentational/user/profile/Profile';
 import PrivateRoute from '../common/privateRoute/PrivateRoute'
-import {createBrowserRouter, Route, RouterProvider} from 'react-router-dom'
+import {createBrowserRouter, Route, RouterProvider, redirect} from 'react-router-dom'
 import { Layout} from 'antd';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import {getCurrentUser} from '../../util/ApiUtils';
+import { ACCESS_TOKEN } from '../../constants';
+import {Loader} from '../common/loader/Loader'
 
 const {Header, Footer, Content} = Layout;
 
@@ -43,16 +46,51 @@ function App() {
 
   const [currentUser, setCurrentUser] = useState();
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const loadCurrentUser = () => {
+    getCurrentUser()
+      .then(response => {
+        setCurrentUser(response);
+        setIsAuthenticated(true);
+        setIsLoading(false);
+      }).catch(error => {
+        setIsLoading(false);
+      })
+  }
+
+  useEffect(() => {
+    loadCurrentUser();
+  }, []);
+
+  const handleLogout = (redirectTo="/") =>{
+    localStorage.removeItem(ACCESS_TOKEN);
+
+    setCurrentUser(null);
+    setIsAuthenticated(false)
+
+    redirect(redirectTo);
+
+    //notification To add
+  }
+
+  const handleLogin = () => {
+    loadCurrentUser();
+    redirect("/");
+  }
+
   return (
+    
     <Layout className="App-container">
       <Header>Header</Header>
      
-
-     <Content>
-     <RouterProvider router={router}/>
-     </Content>
-
+    {isLoading?
+      <Content>
+      <RouterProvider router={router}/>
+      </Content>
+     :
+    <Loader/>
+    }
      <Footer>footer</Footer>
       
     </Layout>
