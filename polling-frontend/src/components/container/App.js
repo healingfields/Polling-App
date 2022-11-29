6b4ff181d9c2adcd7523d8e42861d0975d4aca30
healingfields <dihratene@gmail.com>
2022-11-29 16:13:37 +0100

    updated the app component

--- polling-frontend/src/components/container/App.js
+++ polling-frontend/src/components/container/App.js
@@ -6,15 +6,16 @@ import SignUp from '../presentational/user/signUp/SignUp';
 import Profile from '../presentational/user/profile/Profile';
 import PrivateRoute from '../common/privateRoute/PrivateRoute'
 import {createBrowserRouter, Route, RouterProvider, redirect} from 'react-router-dom'
-import { Layout} from 'antd';
+import { Layout as AntdLayout} from 'antd';
 import { useState, useEffect, useContext } from 'react';
 import {getCurrentUser} from '../../util/ApiUtils';
 import { ACCESS_TOKEN } from '../../constants';
 import {Loader} from '../common/loader/Loader'
 import { Header } from '../common/header/Header';
 import { AuthContext, AuthProvider } from '../../util/authProvider';
+import { Layout } from './Layout';
 
-const {Content, Footer} = Layout;
+const {Content, Footer} = AntdLayout;
 
 function App() {
 
@@ -22,57 +23,40 @@ function App() {
   // const [isAuthenticated, setIsAuthenticated] = useState(false);
   // const [isLoading, setIsLoading] = useState(false);
 
-  const authData = {
-    isLoading:false
-  }
   const router = createBrowserRouter([
     {
       path:"/",
-      element: <PollList />,
-      index:true
+      element: <Layout />,
+      // index:true,
+      children:[
+        {
+          path:"/signup",
+          element:<SignUp/>
+        },
+        {
+          path:"/login",
+          element:<Login
+                   />
+        },
+        {
+          path:"users/:username",
+          element:<Profile
+                    />
+        },
+        {
+          path:"/poll/new",
+          element:<NewPoll/>
+        }
+      ]
     },
-    {
-      path:"/signup",
-      element:<SignUp/>
-    },
-    {
-      path:"/login",
-      element:<Login
-               />
-    },
-    {
-      path:"users/:username",
-      element:<Profile
-                />
-    },
-    {
-      path:"/poll/new",
-      element:
-        <PrivateRoute>
-          <NewPoll/>
-        </PrivateRoute>
-    }
+
   ]);
-
-
-
-
-
-
   return (
-    
-    <Layout className='app-container'>
-      <Header className='app-header'/>
-    {!authData.isLoading?
-      <Content className='app-content'>
-          <RouterProvider router={router}/>
-      </Content>
-     :
-    <Loader/>
-    }
-     <Footer style={{textAlign:'center', color:'white', backgroundColor:'gray'}}>footer</Footer>
-      
-    </Layout>
+      <>
+      <AuthProvider>
+        <RouterProvider router={router}/>
+      </AuthProvider>
+      </>
   );
 }
 
